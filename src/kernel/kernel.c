de15a9a3e669efc11e475774e38ba1762ede41ea
ziole <bhimantara.arsya@gmail.com>
2025-08-03 23:59:29 +0800

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,18 +1,12 @@
 #include "screen.h"
 
-// Deklarasi fungsi untuk menampilkan angka hex
-void print_hex(unsigned int num);
-
 // Kernel entry point
 void kernel_main(unsigned int magic, unsigned int mb_info_addr) {
     clear_screen();
     print("KernelZiole Booted Successfully!\n");
     print("Multiboot Magic: 0x");
-    
-    // Tampilkan magic number dalam hex
     print_hex(magic);
     
-    // Tampilkan alamat info multiboot
     print("\nMultiboot Info: 0x");
     print_hex(mb_info_addr);
     
@@ -23,3 +17,4 @@ void kernel_main(unsigned int magic, unsigned int mb_info_addr) {
         // Kernel idle tasks
     }
 }
+
